c7e31a3f667ff141912bebcc4ca08821e7398f3e
Wagner Ishikawa <wagnwnki@gmail.com>
2020-01-28 22:48:06 -0300

    aula DAO

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,6 @@
+const db = require('../../config/database')
+const LivroDao = require('../infra/livro-dao')
+
 module.exports = (app) => {
 
     app.get('/', function (req, resp) {
@@ -5,12 +8,12 @@ module.exports = (app) => {
         resp.send(
             `
             <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Casa do Código</h1>
-                </body> 
+            <head>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1> Casa do Código</h1>
+            </body> 
             </html>
             `
         );
@@ -18,23 +21,24 @@ module.exports = (app) => {
 
     app.get('/livros', function (req, resp) {
 
-        resp.marko(
-            
-            require('../views/livros/lista/lista.marko'),{
-               
-                livros: [
-                    { 
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    { 
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-        );
-    });
+        const livroDao = new LivroDao(db);
 
-}
+        livroDao.lista()
+
+            .then(livros => resp.marko(
+
+                require('../views/livros/lista/lista.marko'), {
+
+                livros: livros
+
+            }
+            )
+            )
+            .catch(erro => console.log(erro));
+    }
+
+    );
+};
+
+
 
